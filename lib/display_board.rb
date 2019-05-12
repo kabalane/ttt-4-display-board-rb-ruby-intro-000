4dd9f3880f29c6a2916b1bc2dc68001dac81b8e0
Kabalan Elahmadie <kahmadieh@yahoo.com>
2019-05-12 19:45:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 board = [" "," "," "," "," "," "," "," "," "]
-def display_board
+def display_board(board)
   puts "#{board[0]} | #{board[1]} | #{board[2]}"
   puts"-----------"
   puts "#{board[4]} | #{board[5]} | #{board[6]}"
